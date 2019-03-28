526ba46fc9ea236d88249ea31a6724e86b8b7035
lGameWarl <48074048+lGameWarl@users.noreply.github.com>
2019-03-28 15:18:45 +0600

    commit 1

--- src/main/java/com/medium/AppModel.java
+++ src/main/java/com/medium/AppModel.java
@@ -1,10 +1,12 @@
 package com.medium;
 
-import javax.persistence.*;
-import java.io.Serializable;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table
